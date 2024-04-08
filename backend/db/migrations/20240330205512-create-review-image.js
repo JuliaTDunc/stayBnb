89a7e7cbcce703917dbc572e4cb1df2ba3d9e6a0
Julia Duncan <juliataylorduncan@gmail.com>
2024-04-07 22:19:01 -0700

    reviewCaps2

--- backend/db/migrations/20240330205512-create-review-image.js
+++ backend/db/migrations/20240330205512-create-review-image.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('ReviewImages', {
+    await queryInterface.createTable('reviewImages', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -40,7 +40,7 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    options.tableName = 'ReviewImages';
+    options.tableName = 'reviewImages';
     await queryInterface.bulkDelete(options);
   }
 };
