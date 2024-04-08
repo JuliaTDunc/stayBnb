898ca000169fc581537a150f8a85750dab3a2749
Julia Duncan <juliataylorduncan@gmail.com>
2024-04-07 22:35:08 -0700

    reviewCaps

--- backend/app.js
+++ backend/app.js
@@ -69,7 +69,6 @@ app.use((err, _req, res, _next) => {
         title: err.title || 'Server Error',
         message: err.message,
         errors: err.errors,
-        stack: isProduction ? null : err.stack
     });
 });
 
