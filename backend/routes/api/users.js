7c8a256f18a3795c9b9da825657ea30f0d5f6aa2
Julia Duncan <juliataylorduncan@gmail.com>
2024-04-08 11:06:33 -0700

    usersupdate

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -27,26 +27,7 @@ const validateSignup = [
     handleValidationErrors
 ];
 
-router.post(
-    '/',
-    async (req, res) => {
-        const { email, password, username } = req.body;
-        const hashedPassword = bcrypt.hashSync(password);
-        const user = await User.create({ email, username, hashedPassword });
 
-        const safeUser = {
-            id: user.id,
-            email: user.email,
-            username: user.username,
-        };
-
-        await setTokenCookie(res, safeUser);
-
-        return res.json({
-            user: safeUser
-        });
-    }
-);
 router.post(
     '/',
     validateSignup,
