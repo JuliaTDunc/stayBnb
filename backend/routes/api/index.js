ea9375ec6ac364ebfb24c09972cecc75f310c591
Julia Duncan <juliataylorduncan@gmail.com>
2024-04-24 10:54:25 -0700

    first transfer

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,11 +2,6 @@ const router = require('express').Router();
 const { restoreUser } = require("../../utils/auth.js");
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js');
-const bookingsRouter = require('./bookings.js');
-const reviewsRouter = require('./reviews.js');
-const spotImageRouter = require('./spotImage.js');
-const reviewImageRouter = require('./reviewImage.js')
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
@@ -16,15 +11,6 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/spots', spotsRouter);
-
-router.use('/reviews', reviewsRouter);
-
-router.use('/bookings', bookingsRouter);
-
-router.use('/reviewimages', reviewImageRouter);
-
-router.use('/spotimages', spotImageRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
