634c186c26794774579bfb273b9d20d27b7e359b
Julia Duncan <juliataylorduncan@gmail.com>
2024-04-25 20:58:36 -0700

    spts

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,13 +4,13 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-
+const {spotRouter} = require('./spots.js');
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
-
+router.use('/spots', spotRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
