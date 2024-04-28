95b29ef329b9e340ccec4bc676db5a8f743b1990
Julia Duncan <juliataylorduncan@gmail.com>
2024-04-28 13:51:38 -0700

    indx

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,13 +6,19 @@ const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const spotRouter = require('./spots.js');
 const bookingRouter = require('./bookings.js');
+const reviewRouter = require('./reviews.js');
+const reviewImgRouter = require('./reviewImage.js');
+const spotImgRouter = require('./spotImage.js');
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 router.use('/spots', spotRouter);
-router.use('/bookings', bookingRouter)
+router.use('/bookings', bookingRouter);
+router.use('/reviews', reviewRouter);
+router.use('/review-images', reviewImgRouter);
+router.use('/spot-images', spotImgRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
