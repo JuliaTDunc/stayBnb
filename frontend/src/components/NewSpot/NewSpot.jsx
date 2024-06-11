fcebb553db38c7abcb5ff80e4bb0f94c9071420c
Julia Duncan <juliataylorduncan@gmail.com>
2024-06-10 22:42:03 -0700

    Debug-NewSpot

--- frontend/src/components/NewSpot/NewSpot.jsx
+++ frontend/src/components/NewSpot/NewSpot.jsx
@@ -5,86 +5,119 @@ import { useState } from "react";
 
 
 const NewSpot = () => {
-    const [address,setAddress] = useState("");
-    const [city,setCity] = useState("");
-    const [state,setState] = useState("");
-    const [country,setCountry] = useState("");
+   const [formData, setFormData] = useState({address: '', city:'',state:'',country:'',name:'',description:'',price: 80, previewImage:''});
+    
 
-    const handleNewSpotSubmit = () => {
-        
-    }
+   const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData({
+        ...formData,
+        [name]: value
+    })
+   }
+    const handleNewSpotSubmit = async (e) => {
+            e.preventDefault();
+            const spotData = {...formData} //check to see if necessary
+            
+            try{
+                const res = await fetch('/api/spots',{
+                    method: 'POST',
+                    headers:{
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(spotData)
+                });
+                const newSpot = await res.json();
+                console.log(newSpot)
+            } catch (error){
+
+            }
+    };
+    
 
 return (
     <div className='new-form'>
         <h3 className='form-header'>Create a new Spot</h3>
-        <form>
+        <form onSubmit={handleNewSpotSubmit}>
             <section>
                 <h4 className="form-header">Where's your place located?</h4>
                 <h5 className="form-header">Guests will only get your exact address once they booked a<br/>reservation.</h5>
-                <label> Street Address
                     <input type="text"
-                    value={address}
-                    placeholder="Street Address"
-                    onChange={(e) => setAddress(e.target.value)}
-                    required
+                        value={formData.address}
+                        name='address'
+                        placeholder="Street Address"
+                        onChange={handleChange}
+                        required
                     />
-                </label>
-                <label> City
                     <input type="text"
-                    value={city}
-                    placeholder="City"
-                    onChange={(e) => setCity(e.target.value)}
-                    required
+                        value={formData.city}
+                        name='city'
+                        placeholder="City"
+                        onChange={handleChange}
+                        required
                     />
-                </label>
-                <label> State
                     <input type="text"
-                        value={state}
+                        value={formData.state}
+                        name='state'
                         placeholder="State"
-                        onChange={(e) => setState(e.target.value)}
+                        onChange={handleChange}
                         required
                     />
-                </label>
-                <label> Country
                     <input type="text"
-                        value={country}
+                        value={formData.country}
+                        name='country'
                         placeholder="Country"
-                        onChange={(e) => setCountry(e.target.value)}
+                        onChange={handleChange}
                         required
                     />
-                </label>
             </section>
             <section>
                 <h4>Describe your place to guests</h4>
                 <p>Mention the best features of your space, any special amentities like fast wifi or parking, and what you love about the neighborhood.</p>
-                <textarea placeholder="Please write at least 30 characters." required/>
+                <textarea 
+                name='description'
+                value={formData.description}
+                placeholder="Please write at least 30 characters." 
+                onChange={handleChange}
+                required/>
             </section>
             <section>
                 <h4>Create a title for your spot</h4>
                 <p>Catch guests' attention with a spot title that highlights what makes your place special.</p>
-                <label>
-                    <input type="text" placeholder="Name of your spot" required/>
-                </label>
+                    <input type="text"
+                        value={formData.name}
+                        name='name'
+                        placeholder="Title"
+                        onChange={handleChange}
+                        required
+                    />
             </section>
             <section>
                 <h4>Set a base price for your spot</h4>
                 <p>Competitive pricing can help your listing stand out and rank higher in search results.</p>
-                <label>
-                    <input type="number" placeholder="Price per night (USD)" required />
-                </label>
+                    <input type="number"
+                    value={formData.price}
+                     placeholder="Price per night (USD)"
+                     onChange={handleChange}
+                    required />
             </section>
             <section>
                 <h4>Liven up your spot with photos.</h4>
                 <p>Submit a link to at least one photo to publish your spot.</p>
                 <label>
-                    <input type="text" placeholder="Preview Image URL" required />
+                    <input type="text"
+                     placeholder="Preview Image URL"
+                     name='previewImage'
+                     value={FormDataEvent.previewImage}
+                     required
+                     />
                     <input type="text" placeholder="Image URL"/>
                     <input type="text" placeholder="Image URL"/>
                     <input type="text" placeholder="Image URL"/>
                     <input type="text" placeholder="Image URL"/>
                 </label>
             </section>
-            <button onClick={handleNewSpotSubmit}>Create Spot</button>
+            <button type='submit'>Create Spot</button>
         </form>
     </div>
 )
