56172585d5f1229761011e43657474549693047f
Julia Duncan <juliataylorduncan@gmail.com>
2024-06-15 12:41:07 -0700

    csrf cookie bug

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -27,9 +27,12 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 if (process.env.NODE_ENV !== 'production') {
-    router.get('/api/csrf/restore', (req, res) => {
-        res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+    router.get("/api/csrf/restore", (req, res) => {
+        const csrfToken = req.csrfToken();
+        res.cookie("XSRF-TOKEN", csrfToken);
+        res.status(200).json({
+            'XSRF-Token': csrfToken
+        });
     });
 }
 router.get("/api/csrf/restore", (req, res) => {
